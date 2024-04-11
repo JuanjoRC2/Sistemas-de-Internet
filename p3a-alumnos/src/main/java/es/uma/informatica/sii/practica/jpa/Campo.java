ea6f26f0e9200b55903b8f395607114904d2d50c
JuanjoRC2 <juruizca@gmail.com>
2024-04-11 13:53:39 +0200

    asdasd

---
+++ p3a-alumnos/src/main/java/es/uma/informatica/sii/practica/jpa/Campo.java
@@ -0,0 +1,81 @@
+package es.uma.informatica.sii.practica.jpa;
+
+import java.util.Objects;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table (name ="CAMPO")
+public class Campo {
+
+    @Id
+    @Column(name ="ID")
+    private Long id;
+
+    @Column(name= "nombre_campo")
+    private String nombre;
+
+    @Column(name="TIPO")
+    private Tipo tipo;
+
+    @Column(name= "DESCRIPCION")
+    private String descripcion;
+
+    @ManyToOne
+    @JoinColumn(name ="formulario")
+    private Formulario formulario;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Campo campo = (Campo) o;
+        return Objects.equals(id, campo.id) && Objects.equals(nombre, campo.nombre) && tipo == campo.tipo && Objects.equals(descripcion, campo.descripcion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nombre, tipo, descripcion);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Formulario getFormulario() {
+        return formulario;
+    }
+
+    public void setFormulario(Formulario formulario) {
+        this.formulario = formulario;
+    }
+}
